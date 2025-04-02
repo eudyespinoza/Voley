2f052f3649d308a9ab9c47965e1d4dd79035ff3a
Eudy Espinoza <eudyespinoza@gmail.com>
2025-04-02 10:59:15 -0300

    Validacion

--- app.py
+++ app.py
@@ -962,4 +962,4 @@ with app.app_context():
         db.session.commit()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=False, allow_unsafe_werkzeug=True, host='0.0.0.0', port=5000)

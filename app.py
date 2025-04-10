9f1945b77efdaa783edd20ce4785519cbe2ac8f4
Eudy Espinoza <eudyespinoza@gmail.com>
2025-04-10 00:05:53 -0300

    WebSockets

--- app.py
+++ app.py
@@ -962,6 +962,6 @@ with app.app_context():
         db.session.commit()
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host='0.0.0.0', port=5000, log_output=True, cors_allowed_origins="https://www.voleystats.com.ar")
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host='0.0.0.0', port=5001, log_output=True, cors_allowed_origins="https://www.voleystats.com.ar")
 else:
     pass

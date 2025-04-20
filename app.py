ab3b731fd0542a4c81cc4fed86386f65cef3a189
Eudy Espinoza <eudyespinoza@gmail.com>
2025-04-20 10:58:10 -0300

    Tema Black

--- app.py
+++ app.py
@@ -983,4 +983,4 @@ with app.app_context():
         db.session.commit()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
